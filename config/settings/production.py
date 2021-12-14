1cb127be23c68b7528fd24b3af5d4a69fd6d27f2
DESKTOP-GIPHSB6\USER <matsubarashoki@gmail.com>
2021-12-14 11:02:46 +0900

    log用フォルダについて追加

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,3 +3,70 @@ from .base import *
 DEBUG = False
 
 WSGI_APPLICATION = 'config.wsgi.application'
+
+
+#ロギング設定
+LOG_BASE_DIR = BASE_DIR / 'config' / 'logs'
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s [%(levelname)s] %(message)s"
+        },
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    },
+    "handlers": {
+        "console": {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+        "info": {
+            "level": "INFO",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOG_BASE_DIR / "info.log",
+            "formatter": "simple",
+            "maxBytes": 1024 * 512 * 1,  # 1 MB, 
+            "backupCount": 5
+        },
+        "warning": {
+            "level": "WARNING",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOG_BASE_DIR / "warning.log",
+            "formatter": "simple",
+            "maxBytes": 1024 * 512 * 1,  # 1 MB, 
+            "backupCount": 5
+        },
+        "error": {
+            "level": "ERROR",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOG_BASE_DIR / "error.log",
+            "formatter": "simple",
+            "maxBytes": 1024 * 512 * 1,  # 1 MB, 
+            "backupCount": 5
+        }
+    },
+    "loggers": {
+        'django':{
+            "handlers": ["info", "warning", "error"],
+            "level": "INFO",
+        },
+        'youtube_text':{
+            "handlers": ["info", "warning", "error"],
+            "level": "INFO",
+        },
+        'blog':{
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
+    },
+
+}
