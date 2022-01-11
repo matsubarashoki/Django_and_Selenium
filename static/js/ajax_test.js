19dd34f755c4de4f4ead4a5992de0db26ebbeda2
DESKTOP-GIPHSB6\USER <matsubarashoki@gmail.com>
2022-01-12 08:12:18 +0900

    mainデータの取得とテーブル化はできた

--- static/js/ajax_test.js
+++ static/js/ajax_test.js
@@ -5,22 +5,51 @@ $(document).ready(function() {
         dataType: "json",
     }).done(function(data) {
 
-        console.log(data)
-        console.log(data["main"])
-        data.forEach((element) => {
-            var tr = $('<tr />');
-            var td;
-            element.forEach((item, index) => {
-                console.log(item)
-                td = $('<td />').text(item);
-                tr.append(td);
-            });
-            $('#datalist').append(tr)
-        });
-        // $('#datalist').append(tr)
+        console.log(data["main"][0])
+        console.log(data["main"].length)
+
+        var i;
+        // for (i = 0; i > data["main"].length; i++) {
+        //     console(data["main"][i])
+        // }
+        var tr = $('<tr />');
+        var td;
+
+        $.each(data["main"], function(index, value) {
+            if (index == 1) {
+                console.log(value)
+                td = $('<td class="status" />').text(value);
+
+            } else {
+                td = $('<td />').text(value);
+
+            }
+            tr.append(td);
+        })
+        $('#datalist').append(tr)
+        SetStatus()
+
+        var tr = $('<tr />');
+        var modaldiv = $('<div class="modal">')
+        $.each(data["detail"], function(index, value) {
+
+        })
 
     }).fail(function(data) {
 
     });
 
+
 });
+
+function SetStatus() {
+    let select = [];
+    let status = document.getElementsByClassName("status");
+    console.log(status)
+    for (let i = 0; i < status.length; i++) {
+        let selectHtml = "<select id=select" + i + ">";
+        selectHtml = selectHtml + "<option value='1'>" + "稼働中" + "</option>";
+        selectHtml = selectHtml + "<option value='2'>" + "終了" + "</option>";
+        status[i].innerHTML = selectHtml + "</select>";
+    }
+}
