bfe4797f054c024e24e2c78be8a82400df530ac9
DESKTOP-GIPHSB6\USER <matsubarashoki@gmail.com>
2022-01-14 18:33:24 +0900

    うまくいったーー

--- static/js/ajax_test.js
+++ static/js/ajax_test.js
@@ -4,44 +4,57 @@ $(document).ready(function() {
         type: "GET",
         dataType: "json",
     }).done(function(data) {
-
-        var tr;
-        var td;
+        let tr;
+        let td;
         $.each(data, function(index, value) {
             if (index.indexOf('main') !== -1) {
-                var tr = $('<tr />');
-                var td;
-                $.each(value, function(index, m_value) {
-                    if (index == 0) {
-                        //MatterIdは作らない
-                    } else if (index == 2) {
-                        td = $('<td class="status" />').text(m_value);
-                    } else {
-                        td = $('<td />').text(m_value);
+                let tr = $('<tr class="main"/>');
+                let td;
+                $.each(value, function(key, m_value) {
+                    switch (key) {
+                        case "案件ID":
+                            td = $('<td class="matter_id" />').text(m_value);
+                            break;
+                        case "稼働ID":
+                            td = $('<td class="aw_id" />').text(m_value);
+                            break;
+                        case "ステータス":
+                            td = $('<td class="status" />').text(m_value);
+                            break;
+                        default:
+                            td = $('<td />').text(m_value);
+                            break;
                     }
                     tr.append(td);
                 })
                 $('#datalist').append(tr)
 
             } else {
-                var tr_m = $('<tr class="modal_row"/>');
-                var d;
-                $.each(value, function(index, d_value) {
-                    d = $('<div class="modal_item"  />').text(d_value)
-                    tr_m.append(d);
+                let tr_m = $('<tr class="modal_row" nowrap/>');
+                let td = $('<td colspan="7" />');
+                let d;
+                $.each(value, function(key, d_value) {
+                    $(tr_m).attr('id', value["案件ID"] + "_" + value["稼働ID"])
+                    d = $('<div class="modal_item" />').text(key + " : " + d_value)
+                    td.append(d);
                 });
+                tr_m.append(td)
                 $('#datalist').append(tr_m);
             }
-        })
+        });
+        $('#MatterId').css("display", "none");
+        $('.matter_id').css("display", "none");
+        $('.modal_row').hide()
 
         SetStatus();
-        //$('.modal_row').css("display", "none");
-        $('#MatterId').css("display", "none");
 
-    }).fail(function(data) {});
+    }).fail(function(data) {
+
+    });
 
 });
 
+//セレクタボックスの処理
 function SetStatus() {
     let select = [];
     let status = document.getElementsByClassName("status");
@@ -53,7 +66,11 @@ function SetStatus() {
     }
 }
 
-function ShowModal() {
-    let status = document.getElementsByClassName("status");
-
-}
+//詳細行表示切り替えイベント
+$(document).on("click", ".main", function() {
+    //val()はinputとかにしか使えないそうな。知らなかった。
+    //対象行のID取得
+    let target = $(this).children(".matter_id").html() + "_" + $(this).children(".aw_id").html();
+    //表示切り替え 三項演算子
+    let ans = ($('#' + target).css("display") == "none" ? $('#' + target).show() : $('#' + target).hide());
+});
